ae942714119f983b808d5cfe0808822c4d0fdfb3
chm <zmfltm6234@gmail.com>
2026-04-13 15:13:25 +0900

    chm2

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-for i in range(1,10):
-    print(f"---{i}단---")
-    for j in range(1,10):
+print("--- 구구단 프로그램 버전 2.0 ---")
+
+# 반복문을 이용한 출력
+for i in range(1, 10):
+    print(f"\n{i}단:")
+    for j in range(1, 10):
         print(f"{i} * {j} = {i*j}")
+
