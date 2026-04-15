00d59b2a89de7ea7ef154ac8467e94a59a0485fc
choi <your.email@example.com>
2026-04-15 14:26:16 +0900

    chm05

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 print("--- 구구단 프로그램 버전 2.0 ---")
-print("2023333최형민")
 
-for i in range(1, 13):
+k=int(input("정수를 입력:"))
+for i in range(1, k+1):
     print(f"\n{i}단:")
     for j in range(1, 10):
         print(f"{i} * {j} = {i*j}")
